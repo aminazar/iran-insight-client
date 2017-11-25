eb507da2e81e7d376f1db8844abce7e4c2f8cf1d
Eabasir <eabasir@gmail.com>
2017-11-25 19:44:16 +0330

    add to breadcrumb on each click

--- src/app/site/site.routing.ts
+++ src/app/site/site.routing.ts
@@ -5,7 +5,8 @@ import {HomeComponent} from './home/home.component';
 const Site_ROUTES: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'login', loadChildren: 'app/site/login/login.module#LoginModule'}
+  {path: 'login', loadChildren: 'app/site/login/login.module#LoginModule'},
+  {path: 'profile', loadChildren: 'app/site/profile/profile.module#ProfileModule'}
 ];
 
 export const siteRouting = RouterModule.forChild(Site_ROUTES);
