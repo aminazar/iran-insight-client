aac18562b7582e415aea6b11130338864cdb3601
arhariri <ali.71hariri@gmail.com>
2017-11-29 10:34:26 +0330

    Make readable test.module.ts file Some change in test.module.ts

--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -3,27 +3,36 @@ import { CommonModule } from '@angular/common';
 import * as material from '@angular/material';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {RouterTestingModule} from "@angular/router/testing";
-import {HeaderComponent} from '../shared/components/header/header.component';
-import {BreadcrumbComponent} from '../shared/components/breadcrumb/breadcrumb.component';
 import {FormsModule} from '@angular/forms';
-import {BreadcrumbService} from '../shared/services/breadcrumb.service';
-import {AuthService} from '../shared/services/auth.service';
 import {BidiModule} from '@angular/cdk/bidi';
 import {BreadcrumbModule} from 'primeng/primeng';
-import {HomeComponent} from '../site/home/home.component';
-import {ProfileComponent} from '../site/profile/profile.component';
-import {AppComponent} from "../app.component";
-import {AdminHomeComponent} from '../admin/home/home.component';
-import {LoginComponent} from "../admin/login/login.component";
-import {RestService} from "../shared/services/rest.service";
 import {HttpClientModule} from "@angular/common/http";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {SearchService} from "../shared/services/search.service";
-import {SearchFieldsComponent} from "../site/search/search-fields/search-fields.component";
-import {SearchMainComponent} from "../site/search/search-main/search-main.component";
-import {SearchResultComponent} from "../site/search/search-result/search-result.component";
+
+//App
+import {AppComponent} from "../app.component";
+
+//Admin
+import {AdminHomeComponent} from '../admin/home/home.component';
+import {AdminFilterTabComponent} from "../admin/home/filter-tab/filter-tab.component";
+import {LoginComponent} from "../admin/login/login.component";
 import {TypeFormComponent} from "../admin/type/type-form/type-form.component";
 import {TypeComponent} from "../admin/type/type.component";
+
+//Shared Components
+import {BreadcrumbComponent} from '../shared/components/breadcrumb/breadcrumb.component';
+import {HeaderComponent} from '../shared/components/header/header.component';
+import {SearchFieldsComponent} from "../shared/components/search-fields/search-fields.component";
+
+//Shared Services
+import {AuthService} from '../shared/services/auth.service';
+import {BreadcrumbService} from '../shared/services/breadcrumb.service';
+import {RestService} from "../shared/services/rest.service";
+import {SearchService} from "../shared/services/search.service";
+
+//Site
+import {HomeComponent} from '../site/home/home.component';
+import {ProfileComponent} from '../site/profile/profile.component';
 
 
 @NgModule({
@@ -54,13 +63,12 @@ import {TypeComponent} from "../admin/type/type.component";
     AppComponent,
     HeaderComponent,
     AdminHomeComponent,
+    AdminFilterTabComponent,
     BreadcrumbComponent,
     ProfileComponent,
     HomeComponent,
     LoginComponent,
     SearchFieldsComponent,
-    SearchResultComponent,
-    SearchMainComponent,
     TypeFormComponent,
     TypeComponent,
   ],
