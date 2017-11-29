bf0060a3a950bbb791c96ed7dededc76d6bcab18
Eabasir <eabasir@gmail.com>
2017-11-29 11:47:46 +0330

    make children for admin home which are tab views update #12

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,12 +2,16 @@ import {NgModule} from '@angular/core';
 import { AdminHomeComponent } from './home/home.component';
 import {adminRouting} from './admin.routing';
 import {AuthGuard} from "./auth.guard";
+import {MatTabsModule} from '@angular/material';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [AdminHomeComponent],
 
   imports: [
     adminRouting,
+    CommonModule,
+    MatTabsModule
   ],
   providers: [AuthGuard],
   exports: []
