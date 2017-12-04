5d4c3d5f4916b983db2d63c421750a07c0f0625d
Amin Azarbadegan <amin@azarbadegan.net>
2017-12-04 17:01:09 +0330

    Passing all of the tests, skipping as per @ARHariri

--- src/app/admin/person/components/person-expertise/person-expertise.component.spec.ts
+++ src/app/admin/person/components/person-expertise/person-expertise.component.spec.ts
@@ -3,7 +3,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PersonExpertiseComponent } from './person-expertise.component';
 import {TestModule} from '../../../../test/test.module';
 import {AdminTestRouting} from '../../../admin.routing';
-import {MatAutocompleteModule} from '@angular/material';
 
 describe('PersonExpertiseComponent', () => {
   let component: PersonExpertiseComponent;
@@ -11,11 +10,12 @@ describe('PersonExpertiseComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PersonExpertiseComponent ],
+      declarations: [
+        PersonExpertiseComponent,
+      ],
       imports: [
         TestModule,
         AdminTestRouting,
-        MatAutocompleteModule,
       ]
     })
     .compileComponents();
