c405465929cc2c5965f50d3ca01ab2a0b2ef7e2e
arhariri <ali.71hariri@gmail.com>
2018-01-02 16:23:57 +0330

    Semi complete of investment form

--- src/app/admin/investment/components/investment-form/investment-form.component.ts
+++ src/app/admin/investment/components/investment-form/investment-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RestService} from '../../../../shared/services/rest.service';
+import {BreadcrumbService} from '../../../../shared/services/breadcrumb.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'ii-investment-form',
@@ -6,10 +10,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./investment-form.component.css']
 })
 export class InvestmentFormComponent implements OnInit {
+  id = null;
+  investmentForm: FormGroup;
+  loadedValue: any = {};
+  currencyList = [
+    {abbr: 'Afghani', value: 'AFN'},
+    {abbr: 'Euro', value: 'EUR'},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+    {abbr: '', value: ''},
+  ];
+  customCurrency;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private restService: RestService, private breadcrumbService: BreadcrumbService,
+              private router: Router, private route: ActivatedRoute) {
   }
 
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params) => {
+        this.id = params['id'] ? +params['id'] : null;
+        if (this.id)
+          this.getInvestment();
+      },
+      (err) => {
+        console.error('Cannot parse parameters from url: ', err);
+      }
+    );
+  }
+
+  initForm() {
+    this.investmentForm = new FormBuilder().group({
+      amount: [this.loadedValue.amount, [
+        Validators.required,
+      ]],
+      currency: [this.loadedValue.currency, [
+        Validators.required,
+      ]],
+      is_lead: [this.loadedValue.is_lead ? this.loadedValue.is_lead : false, [
+        Validators.required
+      ]],
+    });
+  }
+
+  modifyInvestment() {
+
+  }
+
+  getInvestment() {
+
+  }
 }
--- src/app/admin/investment/investment.module.ts
+++ src/app/admin/investment/investment.module.ts
@@ -3,7 +3,10 @@ import {investmentRouting} from './investment.routing';
 import {InvestmentComponent} from './investment.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { InvestmentFormComponent } from './components/investment-form/investment-form.component';
-import {MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatSnackBarModule} from '@angular/material';
+import {
+  MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatInputModule, MatSelectModule,
+  MatSnackBarModule
+} from '@angular/material';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SharedModule} from '../../shared/shared.module';
 import {CommonModule} from '@angular/common';
@@ -22,6 +25,8 @@ import { InvestmentViewComponent } from './components/investment-view/investment
     MatButtonModule,
     MatDialogModule,
     MatSnackBarModule,
+    MatInputModule,
+    MatSelectModule,
   ],
   declarations: [InvestmentComponent, InvestmentFormComponent, InvestmentViewComponent],
 })
