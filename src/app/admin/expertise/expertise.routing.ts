ee87318e6034c0ecf1b0833962bf5c08d751d304
arhariri <ali.71hariri@gmail.com>
2018-01-07 19:34:36 +0330

    Some improvements to pass tslint errors

--- src/app/admin/expertise/expertise.routing.ts
+++ src/app/admin/expertise/expertise.routing.ts
@@ -1,16 +1,16 @@
-import {RouterModule, Routes} from "@angular/router";
-import {ExpertiseComponent} from "./expertise.component";
-import {ExpertiseFormComponent} from "./expertise-form/expertise-form.component";
-import {ExpertiseViewComponent} from "./expertise-view/expertise-view.component";
-import {LeavingGuard} from "../leavingGuard";
+import {RouterModule, Routes} from '@angular/router';
+import {ExpertiseComponent} from './expertise.component';
+import {ExpertiseFormComponent} from './expertise-form/expertise-form.component';
+import {ExpertiseViewComponent} from './expertise-view/expertise-view.component';
+import {LeavingGuard} from '../leavingGuard';
 
 const Expertise_ROUTES: Routes = [
   {path: '', component: ExpertiseComponent, pathMatch: 'full'},
-  //this shows all the expertises
+  // this shows all the expertises
   {path: ':id', component: ExpertiseViewComponent},
   {path: 'form/:pid/:id', component: ExpertiseFormComponent, canDeactivate: [LeavingGuard]},
-  //if pid == null -> we are creating new expertise by admin
-  //if pid != null -> a person is trying to add new expertise
+  // if pid == null -> we are creating new expertise by admin
+  // if pid != null -> a person is trying to add new expertise
 ];
 
 export const expertiseRouting = RouterModule.forChild(Expertise_ROUTES);
