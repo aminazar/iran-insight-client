97847f50f6aa767974e50df84e6b072caf9a7c15
arhariri <ali.71hariri@gmail.com>
2018-01-14 19:26:11 +0330

    Connect to server to get items with conditions and all categories

--- src/app/admin/externalData/external-data.component.ts
+++ src/app/admin/externalData/external-data.component.ts
@@ -1,73 +1,91 @@
-import {Component, OnInit} from '@angular/core';
-import {MatSnackBar, MatTableDataSource} from '@angular/material';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatSnackBar, MatSort, MatTableDataSource} from '@angular/material';
 import {ExData} from './interfaces/exData.interface';
 import {RestService} from '../../shared/services/rest.service';
 import {ProgressService} from '../../shared/services/progress.service';
 import {SelectionModel} from '@angular/cdk/collections';
-
-const ELData: ExData[] = [
-  {position: 1, name: 'a', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 2, name: 'b', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 3, name: 'c', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 4, name: 'd', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 5, name: 'e', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 6, name: 'f', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 7, name: 'g', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 8, name: 'h', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 9, name: 'i', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 10, name: 'j', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 11, name: 'k', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 12, name: 'l', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 13, name: 'm', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 14, name: 'n', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 15, name: 'o', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 16, name: 'p', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-  {position: 17, name: 'q', type: 'asd', category: 'cat1', market_share: 12.3, province: 'Tehran'},
-];
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'ii-external-data',
   templateUrl: './external-data.component.html',
   styleUrls: ['./external-data.component.css']
 })
-export class ExternalDataComponent implements OnInit {
+export class ExternalDataComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSort) sort: MatSort;
   offset = 0;
   limit = 10;
   phrase = null;
+  category = null;
   totalRecords = null;
   dataSource = null;
   selection = null;
-  displayedColumns = ['select', 'position', 'name', 'type', 'category', 'market_share', 'province'];
+  filteredCategories = [];
+  categoriesList = [];
+  catCtrl: FormControl;
+  displayedColumns = ['select', 'position', 'hhi', 'name', 'type', 'class', 'category', 'market_share', 'province'];
 
   constructor(private restService: RestService, private progressService: ProgressService,
               private snackBar: MatSnackBar) {
   }
 
   ngOnInit() {
+    this.catCtrl = new FormControl();
+    this.catCtrl.valueChanges.subscribe(
+      (data) => {
+        if (data.trim().length === 0) {
+          this.category = null;
+          this.getData();
+        }
+
+        this.filterCategories(data.trim());
+      }
+    );
+
     this.dataSource = new MatTableDataSource<ExData>();
     this.selection = new SelectionModel<ExData>(true, []);
 
     this.getData();
+    this.getCategories();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
   getData() {
-    // this.progressService.enable();
-    // this.restService.post('exData/get' + this.offset + '/' + this.limit, {
-    //   phrase: this.phrase ? this.phrase.trim() : null,
-    // })
-    //   .subscribe(
-    //     (data) => {
-    //       console.log('Data: ', data);
-    //       this.dataSource = data;
-    //       this.progressService.disable();
-    //     },
-    //     (err) => {
-    //       console.error('Cannot get data. Error: ', err);
-    //       this.progressService.disable();
-    //     }
-    //   );
+    this.progressService.enable();
+    this.restService.post('exData/get/' + this.offset + '/' + this.limit, {
+      phrase: this.phrase ? this.phrase.trim() : null,
+      category: this.category ? this.category.trim() : null,
+    })
+      .subscribe(
+        (data) => {
+          this.dataSource.data = data;
+          this.totalRecords = data.length > 0 ? data[0].total : null;
+          this.progressService.disable();
+        },
+        (err) => {
+          console.error('Cannot get data. Error: ', err);
+          this.progressService.disable();
+        }
+      );
+  }
 
-    this.dataSource.data = ELData;
+  getCategories() {
+    this.restService.get('exdata/cat').subscribe(
+      (data) => {
+        this.categoriesList = data.map(el => el.category);
+      },
+      (err) => {
+        console.error('Cannot get categories. Error: ', err);
+      }
+    );
+  }
+
+  filterCategories(cat) {
+    this.filteredCategories = cat ? this.categoriesList.filter(el => new RegExp(cat, 'gi').test(el)) : this.categoriesList.slice();
   }
 
   isAllSelected() {
@@ -90,9 +108,12 @@ export class ExternalDataComponent implements OnInit {
   }
 
   applyFilter(phrase) {
-    if (phrase.trim().length > 2) {
-      this.phrase = phrase;
-      this.getData();
-    }
+    this.phrase = phrase;
+    this.getData();
+  }
+
+  setCategory(cat) {
+    this.category = cat.option.value;
+    this.getData();
   }
 }
